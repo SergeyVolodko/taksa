953cf01b295509f1ea542e6e027cd7758c737752
Sergii Volodko <sergii.volodko@exact.com>
2018-06-04 14:54:05 +0200

    Added ReactJs site project

---
+++ src/Taksa.Site/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using React.AspNet;
+
+namespace Taksa.Site
+{
+	public class Startup
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public IServiceProvider ConfigureServices(IServiceCollection services)
+		{
+
+			services.AddMemoryCache();
+			services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+			services.AddReact();
+
+			return services.BuildServiceProvider();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+
+			// Initialise ReactJS.NET. Must be before static files.
+			app.UseReact(config =>
+			{
+				// If you want to use server-side rendering of React components,
+				// add all the necessary JavaScript files here. This includes
+				// your components as well as all of their dependencies.
+				// See http://reactjs.net/ for more information. Example:
+				//config
+				//  .AddScript("~/Scripts/First.jsx")
+				//  .AddScript("~/Scripts/Second.jsx");
+
+				// If you use an external build too (for example, Babel, Webpack,
+				// Browserify or Gulp), you can improve performance by disabling
+				// ReactJS.NET's version of Babel and loading the pre-transpiled
+				// scripts. Example:
+				//config
+				//  .SetLoadBabel(false)
+				//  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+			});
+
+			app.UseStaticFiles();
+
+			//app.Run(async (context) =>
+			//         {
+			//             await context.Response.WriteAsync("Hello World!");
+			//         });
+		}
+	}
+}
